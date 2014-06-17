832f4762b2599e7a8d8d750665a1fdaa4b006012
Dan Galipo <dan.v.galipo@gmail.com>
2014-06-17 14:38:09 +1000

    Add big blue notice under the list tasks to draw peoples attention to them and that they should be used

--- lib/admin_task_lister.rb
+++ lib/admin_task_lister.rb
@@ -1,5 +1,5 @@
 class AdminTaskLister
-  
+
   def self.list_admin_tasks
     @listed ||= list_tasks_with_rake
   end
@@ -7,9 +7,14 @@ class AdminTaskLister
 protected
 
   def self.list_tasks_with_rake
-    if not Rails.env.development? and is_irb?
+    if !Rails.env.development? && is_irb?
       puts "\e[36m\e[1mAdmin rake tasks available:\e[0m"
       system('rake', '--silent', '--tasks', '^admin')
+      puts "\e[36m\e###############################################################"
+      puts "PLEASE USE THE ABOVE TASKS FOR DATA MANIPULATION/ADDITION"
+      puts "There is a required workflow for making changes to data and"
+      puts "using these tasks will x that you follow it."
+      puts "#############################################################\e[0m"
     end
     true
   end
